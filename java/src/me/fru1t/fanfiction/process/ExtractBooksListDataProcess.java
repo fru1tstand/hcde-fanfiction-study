89e43449922ea6a547b76274dc0c77826e16918e
Kodlee Yin <kodlee@kodleeshare.net>
2016-04-14 01:47:21 -0700

    Code cleanup

--- java/src/me/fru1t/fanfiction/process/ExtractBooksListDataProcess.java
+++ java/src/me/fru1t/fanfiction/process/ExtractBooksListDataProcess.java
@@ -1,5 +1,7 @@
 package me.fru1t.fanfiction.process;
 
+import org.eclipse.jdt.annotation.Nullable;
+
 import me.fru1t.fanfiction.Boot;
 import me.fru1t.fanfiction.database.schema.Scrape;
 import me.fru1t.fanfiction.database.schema.scrape.BufferedRawScrapeProducer;
@@ -8,13 +10,13 @@ import me.fru1t.fanfiction.web.page.BookSearchPage;
 public class ExtractBooksListDataProcess implements Runnable {
 	private static final String PROCESS_SESSION_NAME =
 			"Harry Potter, English, April 8, 2016 - final";
+	@Nullable
 	private static final String[] SCRAPE_SESSION_NAME =
 			{"Harry Potter, English, March 29, 2016"};
 	
 	@Override
 	public void run() {
 		BookSearchPage bsp;
-		
 		BufferedRawScrapeProducer brsp = new BufferedRawScrapeProducer(SCRAPE_SESSION_NAME);
 		Scrape.ScrapeRaw scrape = brsp.take();
 		while (scrape != null) {
