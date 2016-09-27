b188caf7b396faeb2c28ecc8f54467939a9022cf
Kodlee Yin <kodlee@fru1t.me>
2016-09-27 01:32:57 -0700

    Partial completion of initial draft

---
+++ web/php/me/fru1t/research/template/EmptyPage.php
@@ -0,0 +1,51 @@
+<?php
+namespace me\fru1t\research\template;
+use me\fru1t\common\template\Template;
+use me\fru1t\common\template\TemplateField;
+
+/**
+ * Defines an empty page template
+ */
+class EmptyPage extends Template {
+  const FIELD_HTML_TITLE = "html-title";
+  const FIELD_BODY = "body";
+
+  /**
+   * Produces the content this template defines in the form of an HTML string. This method is passed
+   * a map with template field names as keys, and values that the content page provides.
+   *
+   * @param string[] $fields An associative array mapping fields to ContentField objects.
+   * @return string
+   */
+  public static function getTemplateRenderContents_internal(array $fields): string {
+    return <<<HTML
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>Fru1tMe Research - {$fields[self::FIELD_HTML_TITLE]}</title>
+	<meta charset="UTF-8" />
+	
+	<link type="text/css" rel="stylesheet" href="styles.css" />
+	<link type="text/css" rel="stylesheet"
+	      href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css" />
+</head>
+
+<body>
+	{$fields[self::FIELD_BODY]}
+</body>
+</html>
+HTML;
+  }
+
+  /**
+   * Provides the fields this template contains.
+   *
+   * @return TemplateField[]
+   */
+  static function getTemplateFields_internal(): array {
+    return [
+        TemplateField::newBuilder()->called(self::FIELD_HTML_TITLE)->asRequired()->build(),
+        TemplateField::newBuilder()->called(self::FIELD_BODY)->asRequired()->build()
+    ];
+  }
+}
--- web/www/index.php
+++ web/www/index.php
@@ -1,17 +1,43 @@
 <?php
-define("PHP_ROOT", $_SERVER['DOCUMENT_ROOT'] . "/../php");
-require_once PHP_ROOT . "/me/fru1t/common/language/Autoload";
+define("PHP_ROOT", $_SERVER['DOCUMENT_ROOT'] . '/../php');
+require_once PHP_ROOT . '/me/fru1t/common/language/Autoload.php';
 use me\fru1t\common\language\Autoload;
-use me\fru1t\common\language\Session;
-use me\fru1t\common\mysql\MySQL;
-use me\fru1t\common\template\TemplateRouter;
+use me\fru1t\common\language\Http;
+use me\fru1t\common\router\Route;
+use me\fru1t\common\router\Router;
+use me\fru1t\common\template\Templates;
 
-// Autoload MUST be set up first.
 Autoload::setup(PHP_ROOT);
+Router::setup()
+    ->setContentDirectory('../php/me/fru1t/research/content')
+    ->setDefaultContentPagePath('index.php')
+    ->setErrorPagePath('index.php')
+    ->setPageParameterName(Router::DEFAULT_PAGE_PARAMETER_NAME)
+    ->map(Route::newBuilder()
+        ->whenRequested('styles.css')
+        ->provide('../styles/global.css')
+        ->withHeader(Http::HEADER_CONTENT_TYPE_CSS)
+        ->build())
+    ->map(Route::newBuilder()
+        ->whenRequested('global.css.map')
+        ->provide('../styles/global.css.map')
+        ->build())
+    ->map(Route::newBuilder()
+        ->whenRequested('img1.png')
+        ->provide('../static/img1.png')
+        ->withHeader('Content-Type: image/png')
+        ->build())
+    ->map(Route::newBuilder()
+        ->whenRequested('img2.png')
+        ->provide('../static/mariadb-setup.png')
+        ->withHeader('Content-Type: image/png')
+        ->build())
+    ->map(Route::newBuilder()
+        ->whenRequested('start-server.bat')
+        ->provide('../static/start-server.bat')
+        ->withHeader('Content-Disposition: attachment; filename="start-server.bat"')
+        ->build())
+    ->complete();
+Templates::setup()->complete();
 
-// Optionally, other plugins
-Session::setup("my-session-name");
-MySQL::setup("a-host", "a username", "a password", "example database");
-
-// Finally, our template router
-TemplateRouter::openContentFileFromUrl("/../php/me/fru1t/example/", "index.php", "error.php");
+Router::route();
